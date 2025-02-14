4833f359edd7c40b1caffc1055652a82324534a5
Deepak kumar <deepakkumar98111206@gmail.com>
2025-02-15 03:26:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import 'dotenv/config'
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.send("Hello, World!");  
+})
+// const port = 4000
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on port ${process.env.PORT}`)
+}
+)
